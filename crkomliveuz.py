16a0bc070e0af6d8412a8889c9f9150691f9eabe
yovuzlikqiroli <asqaraliyevazizbek963@gmail.com>
2025-03-23 23:04:25 +0500

    Update crkomliveuz.py

--- crkomliveuz.py
+++ crkomliveuz.py
@@ -41,6 +41,9 @@ if machine_code in hash_values_list:
     from telethon.tl.functions.messages import RequestWebViewRequest
     from telethon.tl.functions.account import UpdateStatusRequest
     import csv
+    if "03560274-043C-0513-6506-900700080009" in machine_code:
+        print(colored("❌Aybiz tan olmasez kod ishlamed", "red"))
+        sys.exit()
     print("Oxirgi kod yangilangan vaqti: 03.03.2025 7:09 AM")
     with open(r"C:\join\givid.csv", 'r') as f:
         giv_ids_ozim = [row[0] for row in csv.reader(f)] 
